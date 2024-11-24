2805d26bb439107a9d6d63c0cb0130e5bec6b442
Mi13285 <mixkud95@inbox.ru>
2024-11-24 10:36:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const expendedForm = (n) =>
+  n
+    .toString()
+    .split("")
+    .reverse()
+    .map((a, i) => a * Math.pow(10, i))
+    .filter((a) => a > 0)
+    .reverse()
+    .join(" + ");
+console.log(expendedForm(12));
